9cd655b8a223448670f04f89ac92990c0c498705
Azer <kacek13v2@gmail.com>
2025-02-27 18:53:36 +0100

    starting with pygame.time.Clock

--- main.py
+++ main.py
@@ -7,18 +7,21 @@ from constants import *
 
 def main():
 	pygame.init() #initialize all imported pygame modules
+	pygame.time.Clock
+	dt = 0
+
+
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) #Bierze dane z constants.py
 	print("Starting Asteroids!")
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
-
+#Początek pętli, tzn kodu któru tworzy ekran dla gry ---------------------------------------------
 	while True:
-        	screen.fill((0, 0, 0))  # Wypełniamy ekran czarnym kolorem
-        	pygame.display.flip()    # Odświeżamy ekran
+		screen.fill((0, 0, 0))  # Wypełniamy ekran czarnym kolorem
+		pygame.display.flip()    # Odświeżamy ekran
 #-------------------------------------------------------------------------------------------------
 		for event in pygame.event.get():
-    			if event.type == pygame.QUIT:
-        		return
+			if event.type == pygame.QUIT: return
 #This will check if the user has closed the window and exit the game loop if they do. It will make the window's close button work.
 
 
