7a4901e4d845c7a5de003f2f04a3d47e2ecc3532
Azer <kacek13v2@gmail.com>
2025-03-02 13:05:19 +0100

    Changed how the updatable and drawable works, now they are groups

--- main.py
+++ main.py
@@ -10,19 +10,28 @@ def main():
 	pygame.init() #initialize all imported pygame modules
 	clock = pygame.time.Clock()
 	dt = 0
-	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 #Instacja klasy player  - Tworzy i wyświetla statek gracza (trójkąt). 
-
-
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) #Bierze dane z constants.py
 	print("Starting Asteroids!")
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
-#Początek pętli, tzn kodu któru tworzy ekran dla gry ---------------------------------------------
+#Grupy (https://www.boot.dev/lessons/6a09887c-ad3f-4fb3-8726-c7bd9fa4161c)
+	
+	updatable = pygame.sprite.Group()
+	drawable = pygame.sprite.Group()
+	Player.containers = (updatable, drawable)
+	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2) #gracz
+ 
+
+#Początek pętli, tzn kodu któru tworzy ekran dla gry ---------------------------------------------	
 	while True:
 		screen.fill((0, 0, 0))  # Wypełniamy ekran czarnym kolorem
-		player.draw(screen) #odświeża gracza each frame
-		player.update(dt)
+		
+		updatable.update(dt)
+
+		for entity in drawable: #odświeża gracza each frame
+			entity.draw(screen)
+
 		pygame.display.flip()    # Odświeżamy ekran
 #-------------------------------------------------------------------------------------------------
 		for event in pygame.event.get():
